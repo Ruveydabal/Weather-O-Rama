b2a455499d866af51a9ab96e5302a8a1fb8f45bf
Ruveydabal <ruveydabal024@gmail.com>
2026-09-10 13:43:54 +0200

    make a partens class for weather displays and change the other classes so they work

---
+++ Displays/WeatherDisplay.cs
@@ -0,0 +1,34 @@
+﻿using ObserverPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern.Displays
+{
+    internal abstract class WeatherDisplay : Observer 
+    {
+
+        private float temprature;
+        private float humidity;
+        private float pressure;
+        private Subject weatherData;
+
+        public WeatherDisplay(Subject weatherData)
+        {
+            this.weatherData = weatherData;
+            weatherData.RegisterObserver(this);
+        }
+        public void Update(float temp, float humidity, float pressure) 
+        { 
+            this.temprature = temp;
+            this.humidity = humidity;
+            this.pressure = pressure;
+            Display();
+
+        }
+
+        public abstract void Display();
+    }
+}
